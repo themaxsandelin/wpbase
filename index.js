3194ae071b3144e72f9906f2d82ccf0475126336
Max Sandelin <themaxsandelin@gmail.com>
2018-01-08 16:18:31 +0100

    Updated output with new command and color encoding.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ if (!args.length) return Output.defaultScreen();
 
 const action = args[0];
 if (action === '-h') return Output.helpScreen();
+if (action === '-v') return Output.version();
 if (action === 'init') return Project.initialize();
 
 console.log(action);
--- src/output.js
+++ src/output.js
@@ -8,12 +8,31 @@ function Output() {
     console.log('');
   }
 
-  function helpScreen() {
+  function version() {
+    console.log(JSON.parse(fs.readFileSync(__dirname + '../package.json')).version);
+  }
 
+  function helpScreen() {
+    console.log('');
+    console.log('\x1b[32m%s\x1b[0m', 'WPBase');
+    console.log('');
+    console.log('Usage:');
+    console.log('');
+    console.log('\x1b[32m%s\x1b[0m', '  wpbase init');
+    console.log('  Will initialize your project by analyzing an creating all files and folders (if any are missing).');
+    console.log('');
+    console.log('Options:');
+    console.log('');
+    console.log('\x1b[32m%s\x1b[0m', '  wpbase -h');
+    console.log('  Will show this screen.');
+    console.log('\x1b[32m%s\x1b[0m', '  wpbase -v');
+    console.log('  Will show which version you have installed.');
+    console.log('');
   }
 
   return {
     defaultScreen,
+    version,
     helpScreen
   };
 }
